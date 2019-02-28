56a52a76d5a16c58a5b0f430bbb428b1b2a9b112
Natasha Brayovitch <stanticdoo@gmail.com>
2019-02-28 13:30:28 +0000

    WIP

--- src/Sections/LiveNews.js
+++ src/Sections/LiveNews.js
@@ -3,35 +3,31 @@ import '../App.css';
 
 export default function LiveNews() {
     return (
-        // <div className="Live-News">
-        //     <div className="Live-News-child">
-        //         <iframe title="Live-News-title" className="Live-News-iframe" src="https://newsapi.org/v2/everything?q=bitcoin&apiKey=88e7e18769b44d8e875be42f5b8845d1">
-        //         </iframe>
-        //     </div>
-        //     <div className="Live-News-bottom">
-        //         powered by&nbsp;
-        //     <a href="https://newsapi.org" target="_blank" rel="noopener noreferrer" className="News-feed-link">
-        //             newsapi
-        //     </a>
-        //     </div>
+        <div className="Live-News">
+            <div className="Live-News-child">
+                <iframe title="Live-News-title" className="Live-News-iframe" src="">
+                </iframe>
+            </div>
+            <div className="Live-News-bottom">
+                
+            <a href="https://cashessentials.org/?s=bitcoin" target="_blank" rel="noopener noreferrer" className="News-feed-link">
+                    
+            </a>
+            </div>
        
-        //      <div className="Live-News-overlay">
-        //      <div className="Live-News-child">
-        //        <h1>Tracker Overlay</h1>
-        //        </div>
-        //      <div className="Live-News-bottom">
-        //        powered by&nbsp;
-        //          <a href="https://newsapi.org" target="_blank" rel="noopener noreferrer" className="Live-News-link">
-        //          newsapi
-        //          </a>
-        //      </div>
-        //    </div>
+             <div className="Live-News-overlay">
+             <div className="Live-News-child">
+               <h1>Live News WIP</h1>
+               </div>
+             <div className="Live-News-bottom">
+               
+                 <a href="https://cashessentials.org/?s=bitcoin" target="_blank" rel="noopener noreferrer" className="Live-News-link">
+                 
+                 </a>
+             </div>
+           </div>
            
-        //  </div>
-        <div>
-        <iframe title="NewsFeed" className="NewsFeed-iframe" src="https://content.etoro.com/lp/new-crypto/?gc=1&gclid=Cj0KCQiAh9njBRCYARIsALJhQkH1gOAnE4_kIOvYvbGJHWG1HKAndjmVniDddzGhoWDkX0_QwRc2114aAp4PEALw_wcB&utm_medium=SEM&utm_source=38638&utm_content=0&utm_serial=UK_NB_Crypto_EN_38638|KW_Cryptocurrency|MT_p&utm_campaign=UK_NB_Crypto_EN_38638|KW_Cryptocurrency|MT_p&utm_term=&gclid=Cj0KCQiAh9njBRCYARIsALJhQkH1gOAnE4_kIOvYvbGJHWG1HKAndjmVniDddzGhoWDkX0_QwRc2114aAp4PEALw_wcB" width="100%" height="409" scrolling="auto" marginWidth="0" marginHeight="0" frameBorder="0" border="0">
-          </iframe>
-
-        </div>
+         </div>
+     
     )
 }
